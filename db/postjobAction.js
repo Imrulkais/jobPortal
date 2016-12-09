872e9859871df85abeddeb7b2c8f61bcc417cd51
Naimahmad <naimahmad99@gmail.com>
2016-12-09 14:48:23 +0100

    Post job validation completed

--- db/postjobAction.js
+++ db/postjobAction.js
@@ -9,3 +9,32 @@ var postjobAction = require('../db/postjobAction');
 
 
 
+
+exports.addJob = function(req, res ){
+
+    var usr = req.body;
+    console.log(usr);
+    var details = {
+        companyname: usr.txtCompanyName,
+        jobcategory: usr.cboJobCategory,
+        companyindustrytype: usr.cboJobIndustryType,
+        jobtitle: usr.txtJobTitle,
+        noofvacancies: usr.txtTotalVacancy,
+        jobdetails: usr.txtJobDetails,
+        applyinstructions: usr.txtApplyInstruction,
+        applicationdeadline: usr.txtDeadlineNew,
+        billingcontact: usr.txtBillingContact,
+
+    };
+    db.query('INSERT into  `postnewjob` SET ?', details, function (err, result) {
+        if (err)
+            throw err;
+        else{
+            console.log(' The value inserted. ');
+            req.flash('success', 'Your job has been successfully published.');
+            res.redirect('/postnewjobs');
+        }
+
+    });
+
+};
