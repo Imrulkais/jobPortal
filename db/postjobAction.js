d93d146d12e173f43d56531d9bed94043386c2af
Naimahmad <naimahmad99@gmail.com>
2016-12-09 14:45:23 +0100

    New job post validation in progress

---
+++ db/postjobAction.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Naim on 07-Dec-16.
+ */
+
+var express = require('express');
+var router = express.Router();
+var db = require('../db');
+var postjobAction = require('../db/postjobAction');
+
+
+
